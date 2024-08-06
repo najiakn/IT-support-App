e5541654c56dd24217ecff960b0755bbe1dc54b7
najia <najiaknizi199@gmail.com>
2024-08-06 18:47:20 +0100

    creation table personne et les tables filles

---
+++ support_back/src/main/java/com/example/support_back/model/Admin.java
@@ -0,0 +1,12 @@
+package com.example.support_back.model;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+
+@Entity
+public class Admin extends Personne {
+
+}
---
+++ support_back/src/main/java/com/example/support_back/model/Personne.java
@@ -0,0 +1,23 @@
+package com.example.support_back.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+
+public abstract class Personne {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String mail;
+    private String password;
+
+}
