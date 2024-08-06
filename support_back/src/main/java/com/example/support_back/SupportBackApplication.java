d4549f587bfbc1dc4399d8f31a73f7226a7c23ff
najia <najiaknizi199@gmail.com>
2024-08-06 14:19:51 +0100

    creation projet

---
+++ support_back/src/main/java/com/example/support_back/SupportBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.support_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportBackApplication.class, args);
+    }
+
+}
---
+++ support_back/src/test/java/com/example/support_back/SupportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.support_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
